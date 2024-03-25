654a6330d926d8de0ced33d37186d290504fb653
Jess Ubuntu <jjk14150@gmail.com>
2024-03-24 21:59:40 -0700

    Completed activity

--- code/coding_number_systems.c
+++ code/coding_number_systems.c
@@ -8,32 +8,28 @@ int *toBinary(int input)
 {
     // digits will be entered from right to left, but they might not always fill the array
     // lets enter then from left to right, then reverse, but leave out the null
-    int binaryResult[20];
+    int binaryResult[50];
     int position = 0;
 
     // extract the digits by dividing by 2 and put the remainder into the array
     while (input != 0)
     {
         binaryResult[position] = input % 2; // should be 1 or 0
-        printf("Putting %d into location %d\n", binaryResult[position], position);
         input = input / 2;
-        printf("input is now: %d\n", input);
         position++;
     }
 
-    /* position is +1 higher than the last element in the array
-        because of the way the while loop is written above */
-
-    // Ensure that the binary number is at least 4 digits
-    // if (position - 1 < 4)
-    //     position = 5;
+    /* 
+        position is +1 higher than the last element in the array
+        because of the way the while loop is written above 
+    */
 
     // now flip the array but leave out the NULLS
     // I can start at the end using position
     // from the last while loop
     int higherPointer = position - 1;            // points to the last digit of old array
     int insertPointer = 0;                       // points to the first digit of new returnArray
-    int *returnArray = malloc(sizeof(position)); // define size array in memory to return (size of return + finish signal 2)
+    int *returnArray = malloc(sizeof(int) * 50); // define size array in memory to return (size of return + finish signal 2)
 
     // Fill return array
     while (higherPointer >= 0)
@@ -45,26 +41,8 @@ int *toBinary(int input)
     // Insert a '2' to indicate the end of the array
     returnArray[insertPointer] = 2;
 
-    // Print the binary number
-    printf("Print array in toBinary:\n");
-    int i = 0;
-    while (returnArray[i] != 2)
-    {
-        printf("%d", returnArray[i]);
-        i++;
-    }
-    printf("\n");
-
     return returnArray;
 }
-
-// Convert number to hexadecimal
-
-// Shift number 10 bits to the left
-
-// mask out the lower 10 bits to 0
-
-// logically OR the number of 0x3FF
 
 int main()
 {
@@ -72,11 +50,8 @@ int main()
     int input;
     scanf("%d", &input);
 
-    // I tried to get this to return an array, but I didn't
-    // feel like spending more time figuring out how to
-    // print the array because the sizeof() doesn't work on pointer
     int *binaryArray = toBinary(input);
-    printf("Print array in main:\n");
+    printf("Your number in binary:\n");
     int i = 0;
     while (binaryArray[i] != 2)
     {
@@ -84,4 +59,25 @@ int main()
         i++;
     }
     printf("\n");
+    free(binaryArray);
+
+    printf("Your number in hexidecimal:\n0x%08X\n", input);
+
+    printf("\nApplying shift 10 bits left, mask out lower 10 bits, and logically OR the number 0x3ff\n");
+
+    int result = ((input << 10) & 0xFFFFFC00) | 0x03FF;
+
+    printf("New result in decimal:\n%d\n", result);
+
+    int *newBinaryArray = toBinary(result);
+    printf("New result in binary:\n");
+    i = 0;
+    while (newBinaryArray[i] != 2)
+    {
+        printf("%d", newBinaryArray[i]);
+        i++;
+    }
+    printf("\n");
+
+    printf("New number in hexidecimal:\n0x%08X\n", result);
 }
